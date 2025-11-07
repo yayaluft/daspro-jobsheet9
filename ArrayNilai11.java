ddb0ea2ebb01aa685d24e03a66fefbde2823dd32
yayaluft <syanuha66@gmail.com>
2025-11-07 13:49:45 +0700

    Modifikasi percobaan 1

---
+++ ArrayNilai11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArrayNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i=0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-" +i+ " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" +i+ " adalah "+ nilaiAkhir[i]);
+        }
+    }
+}
