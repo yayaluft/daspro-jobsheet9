9019e3b3378904e7414fed5da7db4cd5ebd3d7f6
yayaluft <syanuha66@gmail.com>
2025-11-07 13:56:58 +0700

    Modifikasi 2 Percobaan 1

--- ArrayBilangan11.java
+++ ArrayBilangan11.java
@@ -2,9 +2,8 @@ public class ArrayBilangan11 {
     public static void main(String[] args) {
 
         int [] bil = {5, 13, -7, 17};
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
