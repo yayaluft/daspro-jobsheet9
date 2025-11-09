327ebacf4f8121ed7257bc8a6b35b58b5c0274fe
yayaluft <syanuha66@gmail.com>
2025-11-09 19:57:49 +0700

    Modifikasi Percobaan 2

--- ArrayNilai11.java
+++ ArrayNilai11.java
@@ -8,8 +8,12 @@ public class ArrayNilai11 {
             System.out.print("Masukkan nilai akhir ke-" +i+ " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" +i+ " adalah "+ nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            }
         }
     }
 }
