b3349e1f453b694a36c8d7a80771ef9500792f0b
yayaluft <syanuha66@gmail.com>
2025-11-11 19:03:05 +0700

    Modifikasi Percobaan 4

---
+++ SearchNilai11.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class SearchNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya yang akan diinput: ");
+        int jumlah = sc.nextInt();
+        int[] arrNilai = new int[jumlah];
+        
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+ ": ");
+            arrNilai[i]= sc.nextInt();
+            }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        int hasil = 0;
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+ (hasil+1));
+    }
+}
